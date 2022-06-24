1f61987af709fdd792ad3fa52c57c26b3f855a5f
OlisaMarvis <Olisamarvis@gmail.com>
2022-06-24 11:39:11 +0100

    Created methods in controller

---
+++ Marvis.BookStore/Marvis.BookStore/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marvis.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"book with id = {id}";
+        }
+
+        public string SearchBooks(string bookName, string authorName)
+        {
+            return $"Book with name = {bookName} & Author = {authorName}";
+        }
+    }
+}
