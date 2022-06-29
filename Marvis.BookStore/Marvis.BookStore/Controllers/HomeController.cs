74ff709c07edae471b225622540bdcfd77d8e9f4
OlisaMarvis <Olisamarvis@gmail.com>
2022-06-29 03:16:12 +0100

    Passing data from controller to view using ViewDataAttribute

--- Marvis.BookStore/Marvis.BookStore/Controllers/HomeController.cs
+++ Marvis.BookStore/Marvis.BookStore/Controllers/HomeController.cs
@@ -5,17 +5,25 @@ namespace Marvis.BookStore.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string CustomProperty { get; set; }
+        [ViewData]
+        public string Title { get; set; }
+        [ViewData]
+        public BookModel Book { get; set; }
         public ViewResult Index()
         {
-            ViewData["property1"] = "Olisa Marvis";
-
-            ViewData["book"] = new BookModel() { Author = "Marvis", Id = 1 };
+            Title = "Home page from controller";
+            CustomProperty = "Custom value";
+            Book = new BookModel() { Id = 1, Title = "Asp.Net Core Tutorial" };
             return View();
             //return View("TempView/MarvisTemp.cshtml");
         }
 
         public ViewResult AboutUs()
         {
+            Title = "About page from controller";
+
             return View();
         }
 
